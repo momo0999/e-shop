15c5659d62ea0fcd06055b2b37b6a64458aad32a
Mohannad Zitoun <64338589+momo0999@users.noreply.github.com>
2021-04-14 22:49:18 +0200

    connect to database mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const colors = require('colors');
+const connectDB = require('./config/db');
 const products = require('./data/products');
 
 dotenv.config();
 
 const app = express();
+
+const startServer = () => {
+  connectDB()
+    .then(() =>
+      app.listen(PORT, () =>
+        console.log(
+          `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+            .yellow.bold
+        )
+      )
+    )
+    .catch((err) => {
+      console.log(err);
+      process.exit(1);
+    });
+};
 
 app.get('/api/products', (req, res) => {
   res.json(products);
@@ -15,7 +33,4 @@ app.get('/api/product/:id', (req, res) => {
   res.json(product);
 });
 const PORT = process.env.PORT || 5000;
-app.listen(
-  PORT,
-  console.log(`Server is running in ${process.env.NODE_ENV} on port ${PORT}...`)
-);
+startServer();
