f7dfe93c317bd8d4d141045347425bd1c2655c81
Mohannad Zitoun <64338589+momo0999@users.noreply.github.com>
2021-04-14 13:13:18 +0200

    initial commit for client

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Hii from the App</h1>
+    </div>
+  );
+}
+
+export default App;
