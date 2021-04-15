91e350509dfd419b09e7086b371538850fa6a234
Mohannad Zitoun <64338589+momo0999@users.noreply.github.com>
2021-04-15 02:44:50 +0200

    create Message and Loader components

--- client/src/screens/ShopScreen.js
+++ client/src/screens/ShopScreen.js
@@ -4,30 +4,37 @@ import { fetchProductsList } from '../actions/productActions';
 import { Col, Row } from 'react-bootstrap';
 
 import Product from '../components/Product';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
 
 const ShopScreen = () => {
   const dispatch = useDispatch();
-  const { products } = useSelector((state) => state.productList);
+  const { products, loading, error } = useSelector(
+    (state) => state.productList
+  );
 
   useEffect(() => {
     dispatch(fetchProductsList());
   }, [dispatch]);
 
-  if (!products) {
-    return;
-  }
   return (
     <React.Fragment>
       <h1>LATEST PRODUCTS</h1>
-      <Row>
-        {products.map((product) => {
-          return (
-            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-              <Product product={product} />
-            </Col>
-          );
-        })}
-      </Row>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
+        <Row>
+          {products.map((product) => {
+            return (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            );
+          })}
+        </Row>
+      )}
     </React.Fragment>
   );
 };
