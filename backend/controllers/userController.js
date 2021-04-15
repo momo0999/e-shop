15cc45b8d25d77aa4116c7fd7aa8d6d604eeffba
Mohannad Zitoun <64338589+momo0999@users.noreply.github.com>
2021-04-16 01:30:07 +0200

    create authMiddleware and getUserProfile route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -22,4 +22,18 @@ const loginUser = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { loginUser };
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
+module.exports = { loginUser, getUserProfile };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { loginUser } = require('../controllers/userController');
+
+const { loginUser, getUserProfile } = require('../controllers/userController');
+const { protect } = require('../middleware/authMiddleware');
 
 router.route('/login').post(loginUser);
+
+router.route('/profile').get(protect, getUserProfile);
 
 module.exports = router;
