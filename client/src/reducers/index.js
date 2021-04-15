670b69393b12d28b015ea0e40b91df83acc0f8e8
Mohannad Zitoun <64338589+momo0999@users.noreply.github.com>
2021-04-15 18:11:30 +0200

    finish shopping cart

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 import { productListReducer, productDetailReducer } from './productReducers';
+import { cartReducer } from './cartReducers';
 
 export default combineReducers({
   productList: productListReducer,
   productDetail: productDetailReducer,
+  cart: cartReducer,
 });
