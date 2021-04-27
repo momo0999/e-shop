90fdf0dab2d394aa1f8e196213c39db7ef5d833d
Mohannad Zitoun <64338589+momo0999@users.noreply.github.com>
2021-04-27 20:28:23 +0200

    create getOrderById endpoint

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 const { protect } = require('../middleware/authMiddleware');
 
-const { addOrderItems } = require('../controllers/orderController');
+const {
+  addOrderItems,
+  getOrderById,
+} = require('../controllers/orderController');
 
 router.route('/').post(protect, addOrderItems);
+router.route('/:id').get(protect, getOrderById);
 
 module.exports = router;
