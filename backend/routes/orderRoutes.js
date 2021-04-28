f5ddd24cecc62452108f3123c41557fd4189b69d
Mohannad Zitoun <64338589+momo0999@users.noreply.github.com>
2021-04-28 22:30:36 +0200

    create order pay endpoint and orderPay reducer and action

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -5,9 +5,11 @@ const { protect } = require('../middleware/authMiddleware');
 const {
   addOrderItems,
   getOrderById,
+  updateOrderToPay,
 } = require('../controllers/orderController');
 
 router.route('/').post(protect, addOrderItems);
 router.route('/:id').get(protect, getOrderById);
+router.route('/:id/pay').put(protect, updateOrderToPay);
 
 module.exports = router;
