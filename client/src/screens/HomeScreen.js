ce682331069568755508ffe7507f8189220d6285
Mohannad Zitoun <64338589+momo0999@users.noreply.github.com>
2021-06-01 10:04:28 +0200

    add top products and finish HomeScreen

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -1,8 +1,22 @@
-import React from 'react';
-import { Carousel } from 'react-bootstrap';
+import React, { useEffect } from 'react';
+import { Button, Card, Carousel, Col, Container, Row } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchTopProducts } from '../actions/productActions';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
+import Product from '../components/Product';
 import { images } from '../homeScreenImages';
 
 const HomeScreen = () => {
+  const dispatch = useDispatch();
+
+  const { products, error, loading } = useSelector(
+    (state) => state.topProducts
+  );
+
+  useEffect(() => {
+    dispatch(fetchTopProducts());
+  }, [dispatch]);
   return (
     <React.Fragment>
       <Carousel className='carousel__homeScreen'>
@@ -22,6 +36,68 @@ const HomeScreen = () => {
           );
         })}
       </Carousel>
+      <div className='section'>
+        <Row className='justify-content-md-center'>
+          <h1>OUR TOP PRODUCTS</h1>
+        </Row>
+        {loading && <Loader />}
+        {error && <Message variant='danger'>{error}</Message>}
+        {products && (
+          <Row className='justify-content-md-center'>
+            {products.map((product) => {
+              return (
+                <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                  <Product product={product} />
+                </Col>
+              );
+            })}
+          </Row>
+        )}
+      </div>
+
+      <Row className='justify-content-md-center'>
+        <h1>Why Choosing Us:</h1>
+      </Row>
+      <div className='flex__row'>
+        <div className='flex__row-col'>
+          <i className='fas fa-recycle fa-4x'></i>
+          <h4>Environmentally Friendly</h4>
+          <p>
+            All our products are environmentally friendly and can be recycled.
+          </p>
+        </div>
+
+        <div className='flex__row-col'>
+          <i className='fas fa-palette fa-4x'></i>
+          <h4>Choose Your Design</h4>
+          <p>
+            We can design your cup as you wish or you send us your own design.
+          </p>
+        </div>
+        <div className='flex__row-col'>
+          <i className='fas fa-shipping-fast fa-4x'></i>
+          <h4>We Deliver Fast</h4>
+          <p>
+            We can deliver in three dayes whether you orderd 1 cup or 100 cups.
+          </p>
+        </div>
+      </div>
+
+      <div className='contact-us'>
+        <p>Have a question?</p>
+        <h1>Get In Touch</h1>
+        <p>
+          Please don't hesitate to contact us if you have any questions, write
+          us an email and we will do our best to get back to you as soon as
+          posible!
+        </p>
+        <a
+          className='btn btn-light my-3 btn-contact'
+          href='mailto:mugshop@example.com'
+        >
+          Contact Us
+        </a>
+      </div>
     </React.Fragment>
   );
 };
