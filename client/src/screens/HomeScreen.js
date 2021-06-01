75fd6d09a3f2f9c8555faa937159c5b35d3b2e0a
Mohannad Zitoun <64338589+momo0999@users.noreply.github.com>
2021-06-01 08:39:13 +0200

    create carousel HomeScreen

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { Carousel } from 'react-bootstrap';
+import { images } from '../homeScreenImages';
 
 const HomeScreen = () => {
   return (
-    <div>
-      <h1>HOMESCREEN</h1>
-    </div>
+    <React.Fragment>
+      <Carousel className='carousel__homeScreen'>
+        {images.map((image, index) => {
+          return (
+            <Carousel.Item interval={2000} pause='hover' key={index}>
+              <img
+                className='d-block w-100'
+                src={image.imgUrl}
+                alt={image.title}
+              />
+              <Carousel.Caption className='carousel__caption-homeScreen'>
+                <h3>{image.title}</h3>
+                <p>{image.desc}</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          );
+        })}
+      </Carousel>
+    </React.Fragment>
   );
 };
 
