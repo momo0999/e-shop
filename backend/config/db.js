c89acdddbe2ad909ce8592f2911cbf4b26a9bb66
Mohannad Zitoun <64338589+momo0999@users.noreply.github.com>
2021-06-04 01:02:09 +0200

    add docker-compose file and env example

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,10 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const DEV_DB_PORT = process.env.DEV_DB_PORT || '27017';
+const MONGO_URI = process.env.MONGO_URI;
 
 const connectDB = async () => {
   try {
@@ -6,7 +12,9 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline);
+    console.log(
+      `MongoDB connected: ${conn.connection.host}:${DEV_DB_PORT}`.cyan.underline
+    );
   } catch (error) {
     console.error(error.message.red.underline.bold);
     process.exit(1);
